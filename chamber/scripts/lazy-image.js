8fed7819cff216003756f57523355d143dddd9b4
tinoemaumbe <122685469+tinoemaumbe@users.noreply.github.com>
2023-10-27 02:05:39 +0200

    Update lazy-image.js

--- chamber/scripts/lazy-image.js
+++ chamber/scripts/lazy-image.js
@@ -28,3 +28,37 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach((image) => {
   imgObserver.observe(image);
 });
+
+<!-- ... previous code ... -->
+
+
+
+  const lazyImages = document.querySelectorAll('.places img[data-src]');
+  const lazyImageObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const lazyImage = entry.target;
+        lazyImage.src = lazyImage.dataset.src;
+        lazyImageObserver.unobserve(lazyImage);
+      }
+    });
+  });
+
+  lazyImages.forEach(lazyImage => {
+    lazyImageObserver.observe(lazyImage);
+  });
+
+  // Calculate and display days between user visits
+  const lastVisitElement = document.querySelector('.lastvisit');
+  const lastVisitTimestamp = localStorage.getItem('lastVisit');
+  if (lastVisitTimestamp) {
+    const lastVisitDate = new Date(lastVisitTimestamp);
+    const currentDate = new Date();
+    const daysBetween = Math.floor((currentDate - lastVisitDate) / (1000 * 60 * 60 * 24));
+    lastVisitElement.textContent = `Days since your last visit: ${daysBetween}`;
+  } else {
+    lastVisitElement.textContent = 'Welcome! This is your first visit.';
+  }
+
+  // Store current visit timestamp in localStorage
+  localStorage.setItem('lastVisit', new Date().toISOString());
