e13ce286a23eea745da49e074c3c7009a70cc80b
tinoemaumbe <122685469+tinoemaumbe@users.noreply.github.com>
2023-10-27 02:23:00 +0200

    Update weather.js

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -29,3 +29,68 @@ fetch(apiURL)
       document.querySelector("#wc").innerHTML = "N/A";
     }
   });
+
+
+
+
+
+
+
+
+async function fetchWeatherForecast() {
+  try {
+    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=c0c96f6b560d532a74ad73b558a917eb=Gweru&days=5');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error fetching weather forecast:', error);
+    return null;
+  }
+}
+
+function updateForecastSection(forecastData) {
+  const forecastSection = document.querySelector('.forecast');
+  if (!forecastData || !forecastSection) return;
+
+  forecastSection.innerHTML = '';
+
+  const forecastTitle = document.createElement('h3');
+  forecastTitle.textContent = 'Weather Forecast';
+
+  const forecastList = document.createElement('ul');
+  forecastList.classList.add('forecast-list');
+
+  forecastData.forecast.forecastday.forEach((day) => {
+    const forecastItem = document.createElement('li');
+    forecastItem.classList.add('forecast-item');
+
+    const date = document.createElement('span');
+    date.classList.add('forecast-date');
+    date.textContent = day.date;
+
+    const condition = document.createElement('span');
+    condition.classList.add('forecast-condition');
+    condition.textContent = day.day.condition.text;
+
+    const temperature = document.createElement('span');
+    temperature.classList.add('forecast-temperature');
+    temperature.textContent = `${day.day.mintemp_c}°C - ${day.day.maxtemp_c}°C`;
+
+    forecastItem.appendChild(date);
+    forecastItem.appendChild(condition);
+    forecastItem.appendChild(temperature);
+
+    forecastList.appendChild(forecastItem);
+  });
+
+  forecastSection.appendChild(forecastTitle);
+  forecastSection.appendChild(forecastList);
+}
+
+fetchWeatherForecast()
+  .then((data) => {
+    updateForecastSection(data);
+  })
+  .catch((error) => {
+    console.log('Error:', error);
+  });
