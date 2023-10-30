37495d201e95aa23391da253f5ab4e27b70c676a
tinoemaumbe <122685469+tinoemaumbe@users.noreply.github.com>
2023-10-30 14:20:30 +0200

    Update main.js

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -53,3 +53,24 @@ window.onresize = () => {
 
   // Update last visit date
   localStorage.setItem('lastVisit', new Date().toString());
+
+
+
+
+ // Update current year
+  const currentYear = new Date().getFullYear();
+  document.getElementById("currentYear").textContent = currentYear;
+
+  // Update last updated date
+  const lastUpdatedDate = new Date(document.lastModified);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  document.getElementById("lastUpdated").textContent = lastUpdatedDate.toLocaleDateString(undefined, options);
+
+  // Handle form submission
+  document.getElementById("join-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const submissionDate = new Date().toLocaleDateString(undefined, options);
+    document.getElementById("submissionDate").textContent = submissionDate;
+    document.getElementById("join-form").style.display = "none";
+    document.getElementById("confirmation").style.display = "block";
+  });
