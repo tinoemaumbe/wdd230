5597cacdca410a9c9ab78b19984a2705586aa304
tinoemaumbe <122685469+tinoemaumbe@users.noreply.github.com>
2023-10-30 14:19:57 +0200

    Update join.js

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -18,3 +18,23 @@ var datetime =
 
 console.log(datetime);
 document.getElementById("submitdate").value = datetime;
+
+
+
+  // Update current year
+  const currentYear = new Date().getFullYear();
+  document.getElementById("currentYear").textContent = currentYear;
+
+  // Update last updated date
+  const lastUpdatedDate = new Date(document.lastModified);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  document.getElementById("lastUpdated").textContent = lastUpdatedDate.toLocaleDateString(undefined, options);
+
+  // Handle form submission
+  document.getElementById("join-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const submissionDate = new Date().toLocaleDateString(undefined, options);
+    document.getElementById("submissionDate").textContent = submissionDate;
+    document.getElementById("join-form").style.display = "none";
+    document.getElementById("confirmation").style.display = "block";
+  });
